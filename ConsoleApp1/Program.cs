0c1d92185a858d47acfa073b2ea0dd14e8587e14
Allen_Chang_tpPC <allen.chang@thinkpower-info.com>
2018-04-25 17:53:17 +0800

    Try1

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ConsoleApp1;
 
 namespace ConsoleApp1
 {
@@ -13,6 +14,11 @@ namespace ConsoleApp1
 
         static void Main(string[] args)
         {
+
+            Calculator cal= new Calculator();
+            int a = cal.add(1,2);
+            Console.WriteLine("1+2={0}",a);
+            Console.Read();
         }
     }
 }
