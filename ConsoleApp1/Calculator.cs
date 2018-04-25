1f45b02124eead0286fb50cd28ae5600f4cdd3be
tpinfo <tpinfo@DESKTOP-0BVT00R>
2018-04-25 17:15:43 +0800

    First commit.

---
+++ ConsoleApp1/Calculator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Calculator
+    {
+
+        public int add(int firstNum, int secondNum)
+        {
+            return firstNum + secondNum;
+        }
+
+        public int Minus(int firstNum, int secondNum)
+        {
+            return firstNum - secondNum +1;
+        }
+    }
+}
---
+++ ConsoleApp1Tests/Class1Tests.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ConsoleApp1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Tests
+{
+    [TestClass()]
+    public class Class1Tests
+    {
+        [TestMethod()]
+        public void Minus_Input_First_3_Second_2_Return_1()
+        {
+            Console.WriteLine("123");
+            Calculator target = new Calculator();
+            int firstNum = 3;
+            int secondNum = 2;
+            int expected = 1;
+            int actual;
+            actual = target.Minus(firstNum, secondNum);
+            Assert.AreEqual(expected, actual);
+            //Assert.Inconclusive("驗證這個方法的正確性。");
+        }
+    }
+}
